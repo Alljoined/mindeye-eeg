d81348d93350bc30b10536b04a101ae70a87a253
alstonlo <40709307+alstonlo@users.noreply.github.com>
2024-01-18 08:36:40 -0500

    device fix

--- mindeye/lit.py
+++ mindeye/lit.py
@@ -150,6 +150,7 @@ class LitMindEye(pl.LightningModule):
         else:
             perm = betas = select = None
 
+        self.clipper.device = self.device  # hack!
         clip_target = self.clipper.embed_image(image).float()
         clip_inputs, clip_inputs_proj = self.prior.eeg2clip(eeg)
 
